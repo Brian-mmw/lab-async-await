9e29f778d35ab20110617881957825970788bd80
brian <mwasbrayo73@gmail.commwasbrayo73@gmail.comclearsudo apt updategit config --global user.email mwasbrayo73@gmail.com>
2026-04-13 13:28:47 +0300

    final fix for async posts lab

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 // Get the UL element
 const ul = document.getElementById('post-list');
 
-// Function to display posts
 function displayPosts(posts) {
-  ul.innerHTML = ""; // clear list
+  ul.innerHTML = "";
 
   posts.forEach(post => {
     const li = document.createElement('li');
@@ -20,20 +19,15 @@ function displayPosts(posts) {
     ul.appendChild(li);
   });
 }
-
-// Async function to fetch data
 async function fetchPosts() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await response.json();
 
-    // 🚨 THIS WAS MISSING
-    displayPosts(data);
-
+    displayPosts(data); // THIS LINE IS CRITICAL
   } catch (error) {
     console.error(error);
   }
 }
 
-// Call the function
 fetchPosts();
