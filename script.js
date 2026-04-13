61320b3a1099fdcea49f3e96142b26bfbe3d01fa
brian <mwasbrayo73@gmail.commwasbrayo73@gmail.comclearsudo apt updategit config --global user.email mwasbrayo73@gmail.com>
2026-04-13 12:17:02 +0300

    complete async lab

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const ul = document.getElementById('post-list');
 
 function displayPosts(posts) {
+  const postList = document.getElementById("post-list");
+
+  // Clear list (good practice)
+  postList.innerHTML = "";
+
   posts.forEach(post => {
     const li = document.createElement('li');
 
@@ -11,20 +16,26 @@ function displayPosts(posts) {
     const p = document.createElement('p');
     p.textContent = post.body;
 
+    // Append elements
     li.appendChild(h1);
     li.appendChild(p);
     ul.appendChild(li);
   });
 }
 
+// Async function to fetch data
 async function fetchPosts() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await response.json();
+
+    // Call display function
     displayPosts(data);
+
   } catch (error) {
     console.error(error);
   }
 }
 
+// Call the function
 fetchPosts();
